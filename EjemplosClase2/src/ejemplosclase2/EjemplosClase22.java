51cf2754f65cba6086285ff71cc0c597e311da00
JoanMBQ <timo_2604@hotmail.com>
2019-04-11 11:10:14 -0500

    Cambios en el codigo

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase22.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author TIMO
+ */
+public class EjemplosClase22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Este metodo es el principal del proyecto
+        
+        String nombre;
+        String apellido;
+        String ciudad;
+        int edad;
+        Scanner entrada = new Scanner(System.in);
+                
+        System.out.println("Por favor ingrese sus nombres");
+        nombre = entrada.nextLine();
+        
+        System.out.println("Por favor ingrese sus apellidos");
+        apellido = entrada.nextLine();
+        
+        System.out.println("Por favor ingrese su edad");
+        edad = entrada.nextInt();
+        entrada.nextLine(); // Limpieza de datos del buffer
+                
+        System.out.println("Por favor Ingrese su ciudad");
+        ciudad = entrada.nextLine();
+        
+        System.out.println("Su nombre es: "+nombre+"\n\n\t Su apellido es: "
+                +apellido+", edad: "+edad+"\n Ciudad: "+ciudad);
+        
+    }
+    
+}
